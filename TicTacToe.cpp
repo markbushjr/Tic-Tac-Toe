0fdb8f26eafb4be3aae423bccb7f5c4da24c7777
markbushjr <132406522+markbushjr@users.noreply.github.com>
2025-02-17 20:12:39 -0500

    Implementation of displayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -17,3 +17,24 @@ TicTacToe::TicTacToe()
         xWins = 0;
         oWins = 0;
 } // TicTacToe
+
+// Function to display the board to the user 
+void TicTacToe::displayBoard()
+{        
+    cout << endl;
+    cout << "Board Reference" << endl;
+    cout << " 0 | 1 | 2 " << endl;
+    cout << "-----------" << endl;
+    cout << " 3 | 4 | 5 " << endl;
+    cout << "-----------" << endl;
+    cout << " 6 | 7 | 8 " << endl;
+    cout << endl;
+
+    cout << " Game Board " << endl;
+    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << " " << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << " " << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << " " << endl;
+    cout << endl;
+} // displayBoard
