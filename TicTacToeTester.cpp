ecb458e2d208b3a78c0964f3d63d0583c44557cd
markbushjr <132406522+markbushjr@users.noreply.github.com>
2025-02-17 20:33:07 -0500

    Test code for checking valid moves

--- TicTacToeTester.cpp
+++ TicTacToeTester.cpp
@@ -17,12 +17,54 @@ void testSwitchPlayer() {
     } // if
 } // testSwitchPlayer
 
+// Function to test valid moves
+void testValidMove() {
+    TicTacToe game;
+
+    if (game.makeMove(0)) 
+    {
+        cout << "testValidMove (valid move) passed!" << endl;
+    } 
+    else 
+    {
+        cout << "testValidMove (valid move) failed!" << endl;
+    } // if
+
+    if (!game.makeMove(0)) 
+    {
+        cout << "testValidMove (already taken) passed!" << endl;
+    } 
+    else 
+    {
+        cout << "testValidMove (already taken) failed!" << endl;
+    } // if
+
+    if (!game.makeMove(-1)) 
+    {
+        cout << "testValidMove (out of bounds) passed!" << endl;
+    } 
+    else 
+    {
+        cout << "testValidMove (out of bounds) failed!" << endl;
+    } // if
+
+    if (!game.makeMove(9)) 
+    {
+        cout << "testValidMove (out of bounds) passed!" << endl;
+    } 
+    else 
+    {
+        cout << "testValidMove (out of bounds) failed!" << endl;
+    } // if
+} // testValidMove
+
 int main() {
     cout << "----------Testing----------" << endl;
     cout << "Starting tests!" << endl;
     cout << endl;
 
     testSwitchPlayer();
+    testValidMove();
 
     cout << endl;
     cout << "All tests completed!" << endl;
