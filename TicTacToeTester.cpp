1804cb8310539096fb33f57a0ec0b84e37256b5f
markbushjr <132406522+markbushjr@users.noreply.github.com>
2025-02-17 20:28:01 -0500

    Test code for switching players

--- TicTacToeTester.cpp
+++ TicTacToeTester.cpp
@@ -2,6 +2,31 @@
 #include "TicTacToe.h"
 using namespace std;
 
+// Function to test switching players
+void testSwitchPlayer() {
+    TicTacToe game;
+    char initialPlayer = game.getCurrentPlayer();
+    game.makeMove(0);
+    if (game.getCurrentPlayer() != initialPlayer) 
+    {
+        cout << "testSwitchPlayer passed!" << endl;
+    } 
+    else 
+    {
+        cout << "testSwitchPlayer failed!" << endl;
+    } // if
+} // testSwitchPlayer
+
 int main() {
+    cout << "----------Testing----------" << endl;
+    cout << "Starting tests!" << endl;
+    cout << endl;
+
+    testSwitchPlayer();
+
+    cout << endl;
+    cout << "All tests completed!" << endl;
+    cout << "----------Testing----------" << endl;
+
     return 0;
 }
